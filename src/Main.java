deb9b96b22f5ceaeac2d7577a97080bcaed47bc3
tiago <up202008790@up.pt>
2025-04-17 15:41:08 -0100

    cleaned main

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,6 @@ import AbstractFactory.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("==== Modelo C1 (Combustão) ====");
-        VehicleFactory combustaoFactory = new CombustaoFactory();
-        Carro c1 = new Carro(combustaoFactory);
-        c1.exibir();
 
-        System.out.println("\n==== Modelo Exclusive (Elétrico) ====");
-        VehicleFactory eletricoFactory = new EletricoFactory();
-        Carro exclusive = new Carro(eletricoFactory);
-        exclusive.exibir();
-
-        System.out.println("\n==== Modelo Transport (Gasoleo) ====");
-        VehicleFactory transportFactory = new TransportFactory();
-        Carro transport = new Carro(transportFactory);
-        transport.exibir();
     }
 }
