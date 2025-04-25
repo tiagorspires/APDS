a22ef348d804ab1713df5b95d0d798c60d0ff57c
tiago <up202008790@up.pt>
2025-04-25 15:53:17 -0100

    adapter

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,6 @@ import TestePratico1.Adapter.*;
 public class Main {
     public static void main(String[] args) {
 
-        Target target = new PayPalAdapter();
-        target.vendacredito(100);
-        target.vendadebito(50);
 
-        Target target2 = new CartaoAdapter();
-        target2.vendacredito(200);
-        target2.vendadebito(150);
     }
 }
